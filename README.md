4b86773ec5c9c04298e242221098543bbdd9b630
NoahKKA <noahhhhh.a@gmail.com>
2023-03-22 19:52:01 -0700

    added player movement

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # MP1
 my first milestone project.
+
+
+https://www.youtube.com/watch?v=rTVoyWu8r6g 
+this youtube video helped me get the skeleton and how to make gravity in my webpage.  Also, helped me with player movements.
--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const canvas = document.querySelector('#gamePage')
 const c = canvas.getContext('2d')
-const gravity = .02;
-//create gravity
+const gravity = .25;
+const keys = {
+    d: {
+        pressed: false,
+    },
+    a: {
+        pressed: false,
+    },
+    w: {
+        pressed: false,
+    },
+}
 
 //made it into a class just incase I want to add more players.
 class Player {
@@ -26,24 +36,60 @@ class Player {
     update() {
         this.draw()
         this.position.y += this.velocity.y
-
+        this.position.x += this.velocity.x
+        //creates gravity
         if(this.velocity.y + this.height + this.position.y < canvas.height){
-            this.velocity.y + gravity
+            this.velocity.y += gravity
         } else {
             this.velocity.y = 0
         }
     }
 }
-
+//creates player
 const player = new Player()
 
 let y = 20;
 function animate(){
     window.requestAnimationFrame(animate)
-    //create canvas
+    //create canvas and refreshes to the browser refresh rate.
     c.fillStyle = 'white'
     c.fillRect(0, 0, canvas.width, canvas.height)
     player.update()
-    //create player
+
+    //makes player stop when keys are not pressed down
+    player.velocity.x = 0
+    if(keys.d.pressed) {
+        player.velocity.x = 1
+    }
+    if(keys.a.pressed){
+        player.velocity.x = -1
+    }
 }
 animate()
+
+//listens as to which key is pressed
+window.addEventListener('keydown', (event) => {
+    console.log(event)
+    if(event.key === 'd'){
+        keys.d.pressed = true
+    }
+    console.log(keys.d.pressed)
+    if(event.key === 'a'){
+        keys.a.pressed = true
+    }
+    if(event.key === 'w'){
+        //only jumps whilst not falling
+        if(player.velocity.y === 0){
+        player.velocity.y = -5
+        }
+    }
+})
+//listens to what key is depressed
+window.addEventListener('keyup', (event) => {
+    if(event.key === 'd'){
+        keys.d.pressed = false
+    }
+    if(event.key === 'a'){
+        keys.a.pressed = false
+    }
+})
